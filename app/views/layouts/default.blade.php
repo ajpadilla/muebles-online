d30b7b8caab205695b7ddce64ca7fbe80d2519a6
Lenyn Alcantara <nightzpy@gmail.com>
2014-10-17 19:48:39 +0000

    Correción en los meta de ta de la página

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
-<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<!--[if lt IE 7 ]><html class="ie ie6" lang="es"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="es"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="es"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="es"> <!--<![endif]-->
 <html lang="es">
 <head>
 	<meta charset="utf-8" />
