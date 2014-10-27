e7d66aa3e5348bc75b0c8e8f2213bb4fe7be7146
Alvaro Padilla <ajpadilla88@gmail.com>
2014-10-27 16:06:20 -0430

    Modificando espacio de los elemetos del formulario

--- app/views/provincias/create.blade.php
+++ app/views/provincias/create.blade.php
@@ -16,8 +16,8 @@
                 <div class="container">
                     <div class="row">
                         <section id="maincontent">
-                            <section id="empty" class="one columns positionleft"></section>
-                            <section id="content" class="ten columns positionleft">
+                            <section id="empty" class="four columns positionleft"></section>
+                            <section id="content" class="four columns positionleft">
                                 <div class="page articlecontainer">
                                     <article class="entry-content">
                                         <h2>Llena los siguientes campos</h2>
@@ -26,16 +26,21 @@
                                           <fieldset>
                                             @include('layouts.partials.error')
                                             <div class="row">
-											  <div class="two_fifth columns">
+											  <div class="four_fifth columns">
 											    {{ Form::label('nombre', 'Nombre:') }}
 											    {{ Form::text('nombre', null, ['size' => '50', 'class' => 'text-input']) }}
 											  </div>
-                                              <div class="two_fifth columns">
-                                                  {{ Form::label('poblacion_id', 'Poblaciones:',array('class'=>'control-label col-md-3')) }}
-                                                  {{ Form::select('poblacion_id',$poblaciones,'',array('class' => 'form-control')) }}
-                                              </div>
-											   <br>
-											    {{ Form::submit('Guardar', ['class' => 'button']) }}
+                                              <div class="four_fifth columns">
+                                                @if(empty($poblaciones))
+                                                    No hay poblaciones registradas
+                                                    <br>
+                                                @else
+                                                    {{ Form::label('poblacion_id', 'Poblaciones:',array('class'=>'control-label col-md-3')) }}
+                                                    {{ Form::select('poblacion_id',$poblaciones,'',array('class' => 'form-control')) }}
+											         <br>
+                                                     {{ Form::submit('Guardar', ['class' => 'button']) }}
+                                                @endif
+											    
 											  </div>
                                             </div>
                                           </fieldset>
