4511b5cf2995105c0fa54a86394182ea68000907
Alvaro Padilla <ajpadilla88@gmail.com>
2014-10-27 14:24:54 -0430

    Agregando modelo para las Poblaciones

--- app/Muebles/Poblaciones/Poblacion.php
+++ app/Muebles/Poblaciones/Poblacion.php
@@ -5,4 +5,9 @@ class Poblacion extends Eloquent{
 	protected $table = 'poblaciones';
 	protected $fillable = ['nombre'];
 
+	public function provincias()
+	{
+		return $this->hasMany('Muebles\Provincia\Provincia');
+	}
+
 }
