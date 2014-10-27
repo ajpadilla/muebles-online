fcee56671123e551e27f02ca0e354b6028c080f6
Alvaro Padilla <ajpadilla88@gmail.com>
2014-10-27 14:27:59 -0430

    Agregando vista para las provincias

---
+++ app/views/provincias/index.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.default')
+@section('content')
+
+<div id="outerafterheader">
+    <div class="container">
+        <div class="row">
+            <div id="afterheader" class="twelve columns">
+                <h3 class="pagetitle nodesc">Registrar nueva población</h3>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- MAIN CONTENT -->
+<div id="outermain">
+    <div id="maincontainer">
+        <div class="container">
+            <div class="row">
+                <section id="maincontent">
+                    <section id="empty" class="one columns positionleft"></section>
+                    <section id="content" class="ten columns positionleft">
+                        <div class="page articlecontainer">
+                            @if (!$provincias->isEmpty())
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>Nombre</th>
+                                        <th>Poblacion</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    @foreach ($provincias as $provincia)
+                                    <tr>
+                                        <td>{{{ $provincia->nombre }}}</td>
+                                        <td>{{{$provincia->poblacion->nombre}}}</td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                            @else
+                            No hay provincias registradas
+                            @endif
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END MAIN CONTENT -->
+    @stop
