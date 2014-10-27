c260e9957b442b9245c4cbc7380d716a8fe5074d
Alvaro Padilla <ajpadilla88@gmail.com>
2014-10-27 14:26:31 -0430

    Modificando el estilos de las tabla para listar la poblaciones

--- app/views/poblaciones/index.blade.php
+++ app/views/poblaciones/index.blade.php
@@ -1,24 +1,53 @@
 @extends('layouts.default')
 @section('content')
-@if (!$poblaciones->isEmpty())
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>Nombre</th>
-            </tr>
-        </thead>
+<div id="outerafterheader">
+    <div class="container">
+        <div class="row">
+            <div id="afterheader" class="twelve columns">
+                <h3 class="pagetitle nodesc">RPoblaciones</h3>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- MAIN CONTENT -->
+<div id="outermain">
+    <div id="maincontainer">
+        <div class="container">
+            <div class="row">
+                <section id="maincontent">
+                    <section id="empty" class="one columns positionleft"></section>
+                    <section id="content" class="ten columns positionleft">
+                        <div class="page articlecontainer">
+                            <article class="entry-content">
+                                <h2>Lista de poblaciones</h2>
+                                <div id="contactform">
+                                 @if (!$poblaciones->isEmpty())
+                                 <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>Nombre</th>
+                                        </tr>
+                                    </thead>
 
-        <tbody>
-            @foreach ($poblaciones as $poblacion)
-                <tr>
-                    <td>{{{ $poblacion->nombre }}}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-@else
-    No hay poblaciones registradas
-@endif
+                                    <tbody>
+                                        @foreach ($poblaciones as $poblacion)
+                                        <tr>
+                                            <td>{{{ $poblacion->nombre }}}</td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                                @else
+                                No hay poblaciones registradas
+                                @endif
 
-
+                            </div><!-- end contactform -->
+                        </article>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- END MAIN CONTENT -->
 @stop
