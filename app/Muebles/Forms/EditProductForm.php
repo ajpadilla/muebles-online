cf4c6ec199d0c2483bd098c6babb1364db8d9f4a
Alvaro Padilla <ajpadilla88@gmail.com>
2014-10-29 14:23:45 -0430

    Agregando reglas para editar productos

---
+++ app/Muebles/Forms/EditProductForm.php
@@ -0,0 +1,22 @@
+<?php namespace Muebles\Forms;
+use Laracasts\Validation\FormValidator;
+
+class EditProductForm extends FormValidator {
+
+	/**
+	 * Validation rules for the form.
+	 *
+	 * @var array
+	 */
+	protected $rules = [
+		'codigo' => 'required|alpha_num|max:128',
+		'nombre' => 'required|max:128',
+		'descripcion' => 'min:5|max:256',
+		'medidas' => 'required|min:5',
+		'precio_lacado' => 'required|max:15',
+		'precio_lacado_puntos' => 'required|max:11',
+		'precio_pulimento' => 'required|max:15',
+		'precio_pulimento_puntos' => 'required|max:11',
+		'precio' => 'required|max:15'
+	];
+}
