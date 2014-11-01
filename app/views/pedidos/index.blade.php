0bbc91729989db5ba054037f5c6d285754e36b7d
Alvaro Padilla <ajpadilla88@gmail.com>
2014-11-01 17:56:41 -0430

    Modificando titulo para la lista de pedidos

--- app/views/pedidos/index.blade.php
+++ app/views/pedidos/index.blade.php
@@ -5,7 +5,7 @@
 	    <div class="container">
 	        <div class="row">
 	            <div id="afterheader" class="twelve columns">
-	                <h1 class="pagetitle nodesc">Lista de pedidos </h1>
+	                <h1 class="pagetitle nodesc">Pedidos de {{$factura->client->nombre}}</h1>
 	            </div>
 	        </div>
 	    </div>
